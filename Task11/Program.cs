6425373e5b244efb9580d32c4145cea8f2151759
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-09-23 19:04:23 +0300

    done task11

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит 
+// случайное трехзначное число
+// удаляет вторую цифру этого числа
+// 765 -> 46
+// 782 -> 82
+// 918 -> 98
+
+int number = new Random().Next(100,1000);
+int Digit(int num)
+{
+    int firstDigit = num / 100 * 10; 
+    int secondDigit = num % 10;
+    return firstDigit + secondDigit;
+}
+int result = Digit(number);
+Console.WriteLine($"{number} -> {result}");
